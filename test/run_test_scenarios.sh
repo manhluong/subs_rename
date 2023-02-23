5fdf33b0e29af69b05ef6bc680f691ff8446c30c
Luong Bui DAZN <luong.bui@dazn.com>
2023-02-23 00:30:25 +0000

    Added case of single movie. Test and renamer.

--- test/run_test_scenarios.sh
+++ test/run_test_scenarios.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-sh ../subs_rename.sh -s ./test_scenario_1 -t srt -c "Italian-it italian-it English-en english-en French-fr french-fr" -d en
+sh ../subs_rename.sh -s ./test_scenario_1 -t srt -c "Italian-it italian-it English-en english-en French-fr french-fr" -d en -l 2
 
-sh ../subs_rename.sh -s ./test_scenario_2 -t srt -c "Italian-it italian-it English-en english-en" -d it
+sh ../subs_rename.sh -s ./test_scenario_2 -t srt -c "Italian-it italian-it English-en english-en" -d it -l 2
+
+sh ../subs_rename.sh -s ./test_scenario_3 -t srt -c "Italian-it italian-it English-en english-en" -d en -l 1
